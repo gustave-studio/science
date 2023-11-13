4b9358e1312311cb3e5fc5aaf41f86c3433671d8
gustave-studio <gustave0222@gmail.com>
2023-11-13 17:09:48 +0900

    ✨トップページを追加
    実装途中。後から少しずつデザインを追加していく。

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import topfunc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', topfunc, name='top'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def topfunc(request):
+  return render(request, 'top.html')
