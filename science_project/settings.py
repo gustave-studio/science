9789a27a953c1f1cef9c3c0f19e5658551d070e5
gustave-studio <gustave0222@gmail.com>
2023-11-15 00:10:12 +0900

    ✨画像ファイルとCSSの設定追加
    画像ファイルとCSSを扱えるようにする。

--- science_project/settings.py
+++ science_project/settings.py
@@ -98,9 +98,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static_url/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 画像ファイルの設定
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media_url/'
