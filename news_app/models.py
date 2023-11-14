3858f7a10696d33e338e10efbaff5779a3bef963
gustave-studio <gustave0222@gmail.com>
2023-11-14 23:37:41 +0900

    ✨NewsModel追加
    ニュースの記事を入れるモデル。

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class NewsModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    newsimage = models.ImageField(upload_to='')
