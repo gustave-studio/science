1d169334598c0688564bcb0caa226dc535c62481
gustave-studio <gustave0222@gmail.com>
2023-11-19 08:03:54 +0900

    ✨ニュース投稿機能追加

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import topfunc
+from .views import topfunc, NewsCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', topfunc, name='top'),
+    path('create/', NewsCreate.as_view(), name='create')
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import NewsModel
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def topfunc(request):
   return render(request, 'top.html')
+
+class NewsCreate(CreateView):
+    template_name = 'news_create.html'
+    model = NewsModel
+    fields = ('title', 'content', 'author', 'newsimage')
+    success_url = reverse_lazy('top')
