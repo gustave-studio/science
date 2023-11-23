31b6d1ae4d6f724a213e4a549c4a1640bcc26f5e
gustave-studio <gustave0222@gmail.com>
2023-11-23 16:29:46 +0900

    ✨forms.pyを使ってフォームを作成

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import topfunc, NewsCreate
+from .views import topfunc, create_news
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', topfunc, name='top'),
-    path('create/', NewsCreate.as_view(), name='create')
+    path('create_news/', create_news, name='create_news')
 ]
